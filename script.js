f64d8d3acc6986c411f111895132a3bdb84a9d8b
Ja'mez Stokes <32825749+iamsostokes@users.noreply.github.com>
2018-01-11 15:53:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 var command="Pick a number between 1 and 8!";
 var user=prompt(command);
 var total=0;
-for (var num=1; num<=8; num++) {
+var wrongnum="incorrect";
+if (user<1){
+  user=prompt(command);
+}
+if (user>=9){
+  user=prompt(command);
+}
+for (var num=1; num<=user; num++) {
   total += num;
 }
-  
+
 console.log(total);
